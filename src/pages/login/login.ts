963c41449a9167af5712dc62d2a353a977c137c2
HemanthNarne <s530945@nwmissouri.edu>
2018-07-10 14:58:32 -0500

    Added validation to the signup and login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StartRegisPage } from '../start-regis/start-regis';
 import { ForgotPasswordPage } from '../forgot-password/forgot-password';
 import { SignupPage } from '../signup/signup';
+// import { FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -17,8 +19,16 @@ import { SignupPage } from '../signup/signup';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  emailId:boolean=false;
+  password:boolean=false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      EmailID: ['', Validators.compose([Validators.pattern('[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]+'), Validators.required])],
+      Password:['',Validators.compose([Validators.minLength(8), Validators.required])]
+      
+    });
   }
 
   ionViewDidLoad() {
@@ -26,8 +36,39 @@ export class LoginPage {
   }
   login(){
     //in future you need to connect Api connections
-    this.navCtrl.push(StartRegisPage);
+    if (!this.loginForm.controls.EmailID.valid || !this.loginForm.controls.Password.valid) 
+      {
+      if (!this.loginForm.controls.EmailID.valid) {
+        this.emailId = true;
+      }
+
+      else if(this.loginForm.controls.EmailID.valid || (this.loginForm.controls.EmailID.touched || this.loginForm.controls.EmailID.dirty))
+      {
+        if (this.loginForm.controls.EmailID.valid && (this.loginForm.controls.EmailID.touched || this.loginForm.controls.EmailID.dirty)) {
+          this.emailId = false;
+        }
+
+      }
+      if (!this.loginForm.controls.Password.valid) {
+        this.password = true;
+      }
+
+      else if(this.loginForm.controls.Password.valid || (this.loginForm.controls.Password.touched || this.loginForm.controls.Password.dirty))
+    {
+      if(this.loginForm.controls.Password.valid && (this.loginForm.controls.Password.touched || this.loginForm.controls.Password.dirty))
+      {
+        this.password=false;
+      }
+
+    } 
   }
+    else
+    {
+      this.navCtrl.push(StartRegisPage);
+    }
+    
+  }
+
 
   signup(){
     this.navCtrl.push(SignupPage);
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LoginPage } from '../login/login';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 /**
  * Generated class for the SignupPage page.
  *
@@ -15,8 +15,20 @@ import { LoginPage } from '../login/login';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  firstName: boolean = false;
+  lastName: boolean = false;
+  emailId: boolean = false;
+  password:boolean=false;
+  confirmpassword:boolean=false;
+  signupForm: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.signupForm = formBuilder.group({
+      firstName: ['', Validators.compose([Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      EmailID: ['', Validators.compose([Validators.pattern('[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]+'), Validators.required])],
+      Password:['',Validators.compose([Validators.minLength(8), Validators.required])],
+      ConfirmPassword:['',Validators.compose([Validators.minLength(8), Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
@@ -25,6 +37,78 @@ export class SignupPage {
 
   signup(){
     //in future you need to connect Api connections
+    if (!this.signupForm.controls.firstName.valid || !this.signupForm.controls.lastName.valid ||
+      !this.signupForm.controls.EmailID.valid || !this.signupForm.controls.Password.valid || !this.signupForm.controls.ConfirmPassword.valid){
+      if (!this.signupForm.controls.firstName.valid) {
+        this.firstName = true;
+      }
+
+      else if(this.signupForm.controls.firstName.valid || (this.signupForm.controls.firstName.touched || this.signupForm.controls.firstName.dirty))
+      {
+        if (this.signupForm.controls.firstName.valid && (this.signupForm.controls.firstName.touched || this.signupForm.controls.firstName.dirty)) {
+          this.firstName = false;
+        }
+
+      }
+      if (!this.signupForm.controls.lastName.valid) {
+        this.lastName = true;
+      }
+
+      else if(this.signupForm.controls.lastName.valid || (this.signupForm.controls.lastName.touched || this.signupForm.controls.lastName.dirty))
+    {
+      if(this.signupForm.controls.lastName.valid && (this.signupForm.controls.lastName.touched || this.signupForm.controls.lastName.dirty))
+      {
+        this.lastName=false;
+      }
+
+    } 
+
+      if (!this.signupForm.controls.EmailID.valid) {
+        this.emailId = true;
+      }
+
+      else if(this.signupForm.controls.EmailID.valid || (this.signupForm.controls.EmailID.touched || this.signupForm.controls.EmailID.dirty))
+    {
+      if(this.signupForm.controls.EmailID.valid && (this.signupForm.controls.EmailID.touched || this.signupForm.controls.EmailID.dirty))
+      {
+        this.emailId=false;
+      }
+
+    } 
+
+    if (!this.signupForm.controls.Password.valid) {
+      this.password = true;
+    }
+
+    else if(this.signupForm.controls.Password.valid || (this.signupForm.controls.Password.touched || this.signupForm.controls.Password.dirty))
+  {
+    if(this.signupForm.controls.Password.valid && (this.signupForm.controls.Password.touched || this.signupForm.controls.Password.dirty))
+    {
+      this.password=false;
+    }
+
+  } 
+
+  if (!this.signupForm.controls.ConfirmPassword.valid) {
+    this.confirmpassword = true;
+  }
+
+  else if(this.signupForm.controls.ConfirmPassword.valid || (this.signupForm.controls.ConfirmPassword.touched || this.signupForm.controls.ConfirmPassword.dirty))
+{
+  if(this.signupForm.controls.ConfirmPassword.valid && (this.signupForm.controls.ConfirmPassword.touched || this.signupForm.controls.ConfirmPassword.dirty))
+  {
+    this.confirmpassword=false;
+  }
+
+} 
+    
+
+  } 
+
+  else
+  {
     this.navCtrl.push(LoginPage);
   }
+  
+}
 }
