57cc7e0e1b58d5d14032c91b99d70c7951e3e6ef
S530488@nwmissouri.edu <TDqwXa2d4>
2018-11-26 11:46:17 -0600

    Made minor change regarding the root page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // rootPage: any = ReportDisasterPage;
-  rootPage: any = RegisFormPage ;
+  rootPage: any = LoginPage ;
   // StartRegisPage
   // LoginPage ;
   // rootPage: any =  RegisFormPage;
